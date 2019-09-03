286a327677716ad06e2b9bb111710387acc14837
craigz <craigz@gmail.com>
2019-09-03 12:15:29 -0400

    created repo for project.

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine","pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req,res) => {
+//	res.status(200).send("WHATBYTE: food for devs (and other hungry humans).");
+	res.render("index", { title: "Home"});
+});
+
+app.listen(port, () => {
+	console.log(`Listening to requests on http://localhost:${port}`);
+});
