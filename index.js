7029c9e220e523a9fe4db4b67bf8da4d2b857d71
craigz <craigz@gmail.com>
2019-09-19 22:36:33 -0400

    create auth.js and added routes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // index.js
-
+require ("dotenv").config();
 const path = require("path");
 const express = require("express");
 const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+const authRouter = require("./auth");
 
 const app = express();
 const port = process.env.PORT || "8000";
@@ -14,15 +17,56 @@ const session = {
   saveUninitialized: false
 };
 
+const strategy = new Auth0Strategy(
+ {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API 
+     * (resource server)
+     * accessToken is the token to call the Auth0 API 
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
 if (app.get("env") === "production") {
   session.cookie.secure = true; // Serve secure cookies, requires HTTPS
 }
 
 app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine","pug");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
+
+app.use("/", authRouter);
 
 app.get("/", (req,res) => {
 //	res.status(200).send("WHATBYTE: food for devs (and other hungry humans).");
@@ -30,7 +74,7 @@ app.get("/", (req,res) => {
 });
 
 app.get("/user", (req, res) => {
-  res.render("user", { title: "Profile", userProfile: { nickname: "craigz" } });
+  res.render("user", { title: "Profile", userProfile: { nickname: "WHATABYTE" } });
 });
 
 app.listen(port, () => {
